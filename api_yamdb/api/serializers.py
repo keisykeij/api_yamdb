31ef37a3865e9eb08a79f480b8093618bf4df1be
Nikolay Gerasimov <nikolay.gerasimov495@yandex.ru>
2024-06-20 20:22:18 +0300

    fix reviews

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,15 +3,6 @@ from rest_framework import serializers
 from reviews.models import Category, Genre, Title
 
 
-class TitleSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели Title."""
-
-    class Meta:
-        fields = ('id', 'name', 'year', 'rating', 'description',
-                  'genre', 'category')
-        model = Title
-
-
 class CategorySerializer(serializers.ModelSerializer):
     """Сериализатор для модели Category."""
 
@@ -26,3 +17,17 @@ class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('name', 'slug')
         model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Title."""
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(
+        read_only=True,
+        many=True
+    )
+
+    class Meta:
+        fields = ('id', 'name', 'year', 'rating', 'description',
+                  'genre', 'category')
+        model = Title
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -6,12 +6,10 @@ from .vaidators import validate_year
 class Title(models.Model):
     name = models.CharField('Название', max_length=256, db_index=True)
     year = models.IntegerField('Год выпуска', validators=(validate_year,))
-    description = models.TextField('Описание', max_length=256,
-                                   null=True, blank=True)
+    description = models.TextField('Описание', null=True, blank=True)
     genre = models.ManyToManyField('Genre', verbose_name='Жанр',
-                                   related_name='titles', null=True, blank=True)
-    category = models.ForeignKey('Category', on_delete=models.SET_NULL,
-                                 null=True, blank=True)
+                                   related_name='titles')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL)
 
     class Meta:
         verbose_name = 'Произведение'
