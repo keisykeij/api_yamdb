3dd7dcbbfab7e365f0b7f2f5c2af6c29c2954247
Артём Щукин <artemschukin@MacBook-Pro-Artem.local>
2024-06-22 20:16:35 +0300

    Поправил замечания.

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -35,13 +35,13 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
-            or request.user.role == ROLES
+            or request.user.role == ROLES.admin.value
         )
 
     def has_object_permission(self, request, view, obj):
         return request.user.role == ROLES.admin.value
 
 
-class ProfilePermissions(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.role == ROLES.admin.value
+# class ProfilePermissions(permissions.BasePermission):
+#     def has_permission(self, request, view):
+#         return request.user.role == ROLES.admin.value
