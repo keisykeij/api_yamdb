20952310ccef0d7cc2e222162c89b5ba3f0bc696
Артём Щукин <artemschukin@MacBook-Pro-Artem.local>
2024-06-22 17:03:10 +0300

    Реализованы permissions для проекта.

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -4,9 +4,17 @@ from .views import UserRegisterView, UserTokenView
 
 app_name = 'users'
 
+API_V1 = 'v1'
+
 urlpatterns = [
     path(
-        'v1/auth/signup/', UserRegisterView.as_view(), name='user_signup'
+        f'{API_V1}/auth/signup/',
+        UserRegisterView.as_view(),
+        name='user_signup'
     ),
-    path('v1/auth/token/', UserTokenView.as_view(), name='user_token'),
+    path(
+        f'{API_V1}/auth/token/',
+        UserTokenView.as_view(),
+        name='user_token'
+    ),
 ]
