c000ee3648b61163a1b7a8d99e2c739331970158
Артём Щукин <artemschukin@MacBook-Pro-Artem.local>
2024-06-24 23:40:27 +0300

    Add simplejwt to installed_apps.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'api.apps.ApiConfig',
     'reviews.apps.ReviewsConfig',
     'users.apps.UsersConfig',
